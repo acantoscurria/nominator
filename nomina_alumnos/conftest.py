60b6aea728c2924e8089a38baf6e447798f257b4
Juan Ledesma <juanjoseledesma90@gmail.com>
2022-08-26 08:22:39 -0300

    commit inicial para desarrollo de formualario

---
+++ nomina_alumnos/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from nomina_alumnos.users.models import User
+from nomina_alumnos.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
